acc8c8510b0ad6bba13adeece02cf10b074cab08
Mike Gretton <mikegretton@gmail.com>
2020-05-05 23:29:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(ppl, instr) {
+  let arr = [];
+  for (let i = 0; i < ppl.length; i++) {
+    arr.push(`${ppl[i]} plays ${instr[i]}`);
+  }
+  return arr;
+}
